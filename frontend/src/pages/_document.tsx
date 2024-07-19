42199d71c743cf7231add4fa237faba5d89a8146
Samuel Faber <samuel.faber@live.fr>
2024-07-19 09:41:35 +0200

    🎨 Update _document.tsx to include a title tag

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        <title>ShortURL</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
