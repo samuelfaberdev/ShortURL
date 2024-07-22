61c7cdddc0a3228c7628764293634db3e09438f1
Samuel Faber <samuel.faber@live.fr>
2024-07-22 11:07:34 +0200

    🐛 Remove title tag from _document.tsx

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head>
-        <title>ShortURL</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
