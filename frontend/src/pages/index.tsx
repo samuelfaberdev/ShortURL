1a3c53b355b65675f777ada98fca28e906b77f29
Samuel Faber <samuel.faber@live.fr>
2024-07-22 11:08:11 +0200

    ✨ Add logo and Toaster component to homepage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,23 @@
 import { ShortURLForm } from "@/components/ShortURLForm";
+import { Toaster } from "@/components/ui/toaster";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <ShortURLForm />
-    </main>
+    <>
+      <Head>
+        <title>ShortURL</title>
+      </Head>
+      <main
+        className={`flex min-h-screen flex-col items-center gap-4 p-24 ${inter.className}`}
+      >
+        <img src="/logo.png" className="h-14" height={56} />
+        <ShortURLForm />
+        <Toaster />
+      </main>
+    </>
   );
 }
