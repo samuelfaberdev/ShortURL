d8dd991ce0174a64c2d34b138ad4cb68d3cef2c0
Samuel Faber <samuel.faber@live.fr>
2024-07-23 15:42:18 +0200

    🎨 Add Layout component and update index page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,23 +1,12 @@
+import Layout from "@/components/Layout";
 import { ShortURLForm } from "@/components/ShortURLForm";
-import { Toaster } from "@/components/ui/toaster";
-import { Inter } from "next/font/google";
-import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
+import { SignupCard } from "@/components/SignupCard";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>ShortURL</title>
-      </Head>
-      <main
-        className={`flex min-h-screen flex-col items-center gap-4 p-6 sm:p-12 lg:p-24 ${inter.className}`}
-      >
-        <img src="/logo.png" className="h-14" height={56} />
-        <ShortURLForm />
-        <Toaster />
-      </main>
-    </>
+    <Layout title="ShortURL">
+      <ShortURLForm />
+      <SignupCard />
+    </Layout>
   );
 }
