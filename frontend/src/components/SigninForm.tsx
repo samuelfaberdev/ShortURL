5ed4ce301056eaecad3dce1f922b04f1e87b5b50
Samuel Faber <samuel.faber@live.fr>
2024-07-23 18:04:07 +0200

    🚀 Deploy test

--- frontend/src/components/SigninForm.tsx
+++ frontend/src/components/SigninForm.tsx
@@ -61,7 +61,7 @@ export function SigninForm() {
         // setTimeout(() => {
         //   router.replace("/dashboard");
         // }, 2000);
-        //
+        // Test
       } else {
         throw new Error("Mauvais identifiants !");
       }
