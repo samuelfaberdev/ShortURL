b8c7ef0f0bc87c54e35baae16f6f1d258a8e71d9
Samuel Faber <samuel.faber@live.fr>
2024-07-23 15:43:59 +0200

    ✨ Add signup page and related components

---
+++ frontend/src/pages/signup.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+import { SignupForm } from "@/components/SignupForm";
+
+export default function Signup() {
+  return (
+    <Layout title="ShortURL - Inscription">
+      <SignupForm />
+    </Layout>
+  );
+}
